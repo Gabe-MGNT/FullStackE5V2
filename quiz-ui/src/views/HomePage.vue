cb1df3e7ec1bf48ba5c1c12931bf1e6c423eb617
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2023-12-19 13:48:01 +0100

    add first ui page

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import quizApiService from "@/services/QuizApiService";
+
+const registeredScores = ref([]);
+
+onMounted(async () => {
+		console.log("Home page mounted");
+    registeredScores.value = quizApiService.getQuizInfo();
+});
+
+
+</script>
+
+<template>
+  <h1>Home page</h1>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+  {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+
+  <router-link to="/new-quiz">Démarrer le quiz !</router-link>
+</template>
